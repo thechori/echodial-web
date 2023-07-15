58f4bbbe206403950284b298ad2c1157dcdc864c
Ryan Teodoro <ryan.h.teodoro@gmail.com>
2023-07-15 00:28:33 -0500

    make header responsive

--- src/components/header/Header.styles.ts
+++ src/components/header/Header.styles.ts
@@ -1,10 +1,58 @@
 import styled from "styled-components";
+import devices from "../../styles/devices";
 
 const HeaderStyled = styled.div`
   background-color: #2a2a2a;
   height: 100px;
   display: flex;
   align-items: center;
+
+  .full-menu {
+    display: none;
+
+    @media ${devices.tablet} {
+      display: block;
+    }
+  }
+
+  .hamburger-menu {
+    display: block;
+    padding: 1rem;
+
+    @media ${devices.tablet} {
+      display: none;
+    }
+
+    &:hover {
+      cursor: pointer;
+      opacity: 0.6;
+    }
+  }
+
+  .hamburger-menu-drawer {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    background-color: rgba(0, 0, 0, 0.7);
+
+    .hamburger-menu-drawer-links {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+      align-items: flex-end;
+
+      a {
+        font-size: 2rem;
+        padding: 1.5rem;
+      }
+    }
+  }
 
   .header-container {
     display: flex;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 //
 import HeaderStyled from "./Header.styles";
 import leadsLogoFull from "../../assets/l34ds-logo-full.png";
 
 const Header = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleClickOff = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    setExpanded(false);
+  };
+
   return (
     <HeaderStyled>
       <div className="container">
@@ -12,10 +20,30 @@ const Header = () => {
             <img src={leadsLogoFull} alt="L34ds company logo" />
           </div>
           <div className="links">
-            <Link to="/">Home</Link>
-            <Link to="/sign-in">Sign in</Link>
-            <Link to="/sign-up">Sign up</Link>
-            <Link to="/settings">Settings</Link>
+            <div
+              className="hamburger-menu"
+              onClick={() => setExpanded(!expanded)}
+            >
+              🍔
+            </div>
+
+            {expanded && (
+              <div className="hamburger-menu-drawer" onClick={handleClickOff}>
+                <div className="hamburger-menu-drawer-links">
+                  <Link to="/">Home</Link>
+                  <Link to="/sign-in">Sign in</Link>
+                  <Link to="/sign-up">Sign up</Link>
+                  <Link to="/settings">Settings</Link>
+                </div>
+              </div>
+            )}
+
+            <div className="full-menu">
+              <Link to="/">Home</Link>
+              <Link to="/sign-in">Sign in</Link>
+              <Link to="/sign-up">Sign up</Link>
+              <Link to="/settings">Settings</Link>
+            </div>
           </div>
         </div>
       </div>
