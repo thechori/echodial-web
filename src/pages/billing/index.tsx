ad8653449cd7ae043cd6ababe0eb421740fd5d32
Ryan Teodoro <ryan.h.teodoro@gmail.com>
2023-07-27 13:37:12 -0500

    add charting library

--- src/pages/billing/index.tsx
+++ src/pages/billing/index.tsx
@@ -4,6 +4,7 @@ import { useAppDispatch } from "../../store/hooks";
 import { signOut } from "../../store/user/slice";
 import routes from "../../configs/routes";
 import BillingStyled from "./Billing.styles";
+import { Box, Card, Title } from "@mantine/core";
 
 function Billing() {
   const dispatch = useAppDispatch();
@@ -19,6 +20,19 @@ function Billing() {
       <div className="container">
         <h1>Billing</h1>
         <p>Here are some Billing</p>
+        <Card>
+          <Title order={2} mb={16}>
+            Balance History
+          </Title>
+          <Box></Box>
+        </Card>
+        <Card>
+          <Title order={2} mb={16}>
+            Usage History
+          </Title>
+
+          <Box></Box>
+        </Card>
         <button onClick={handleSignOut}>Sign out</button>
       </div>
     </BillingStyled>
