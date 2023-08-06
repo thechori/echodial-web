91d2aca75dbba4a67e9a9d03d903c51324e9340e
Ryan Teodoro <ryan.h.teodoro@gmail.com>
2023-08-05 19:14:46 -0500

    add ag-grid and mock data to leads page

--- src/pages/leads/index.tsx
+++ src/pages/leads/index.tsx
@@ -1,18 +1,59 @@
+import { useState, useRef, useEffect, useMemo, useCallback } from "react";
+import { AgGridReact } from "ag-grid-react"; // the AG Grid React Component
+import { Box, Container, Title } from "@mantine/core";
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
+//
 import LeadsStyled from "./Leads.styles";
 
 function Leads() {
+  const gridRef = useRef<any>(); // Optional - for accessing Grid's API
+  const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
+
+  // Each Column Definition results in one Column.
+  const [columnDefs] = useState([
+    { field: "make", filter: true },
+    { field: "model", filter: true },
+    { field: "price" },
+  ]);
+
+  // DefaultColDef sets props common to all Columns
+  const defaultColDef = useMemo(
+    () => ({
+      sortable: true,
+    }),
+    []
+  );
+
+  // Example of consuming Grid Event
+  const cellClickedListener = useCallback((event: any) => {
+    console.log("cellClicked", event);
+  }, []);
+
+  // Example load data from server
+  useEffect(() => {
+    fetch("https://www.ag-grid.com/example-assets/row-data.json")
+      .then((result) => result.json())
+      .then((rowData) => setRowData(rowData));
+  }, []);
+
   return (
     <LeadsStyled>
-      <div className="container">
-        <h1>Leads</h1>
-        <p>Here are some Leads</p>
-        <p>Lead #1</p>
-        <p>Lead #1</p>
-        <p>Lead #1</p>
-        <p>Lead #1</p>
-        <p>Lead #1</p>
-        <p>Lead #1</p>
-      </div>
+      <Container py="xl">
+        <Title order={2}>Leads</Title>
+
+        <Box className="ag-theme-alpine" style={{ width: 500, height: 500 }}>
+          <AgGridReact
+            ref={gridRef} // Ref for accessing Grid's API
+            rowData={rowData} // Row Data for Rows
+            columnDefs={columnDefs} // Column Defs for Columns
+            defaultColDef={defaultColDef} // Default Column Properties
+            animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+            rowSelection="multiple" // Options - allows click selection of rows
+            onCellClicked={cellClickedListener} // Optional - registering for Grid Event
+          />
+        </Box>
+      </Container>
     </LeadsStyled>
   );
 }
