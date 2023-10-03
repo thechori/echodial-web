be4f1787ae321556583b7382e77dc3a3bd00828d
Ryan Teodoro <ryan.h.teodoro@gmail.com>
2023-10-03 00:07:24 -0500

    improve ui of features page

--- src/pages/features/index.tsx
+++ src/pages/features/index.tsx
@@ -5,7 +5,6 @@ import {
   Center,
   Container,
   List,
-  Text,
   ThemeIcon,
   Title,
 } from "@mantine/core";
@@ -20,48 +19,43 @@ function Features() {
   return (
     <FeaturesStyled>
       <Container py="xl">
-        <Card withBorder shadow="md" p="lg" maw={400} mx="auto">
-          <Title order={1} weight={200} mb="md">
-            Features
-          </Title>
+        <Card withBorder shadow="md" p="xl" mx="auto" maw={800}>
+          <Box maw={400} mx="auto">
+            <Title order={1} weight={200} mb="md">
+              Features
+            </Title>
 
-          <Box>
-            <List
-              ta="left"
-              spacing="sm"
-              icon={
-                <ThemeIcon color="teal" size={24} radius="xl">
-                  <IconCircleCheck size="1rem" />
-                </ThemeIcon>
-              }
-            >
-              <List.Item>Automatic dialer</List.Item>
-              <List.Item>
-                Verified calls with YOUR phone number (
-                <Text span italic weight={500} color="blue">
-                  no more blocked calls
-                </Text>
-                )
-              </List.Item>
-              <List.Item>Mobile friendly -- call on the go!</List.Item>
-              <List.Item>
-                1000 minutes included. Purchase more anytime
-              </List.Item>
-              <List.Item>100% satisfaction guarantee</List.Item>
-            </List>
+            <Box>
+              <List
+                ta="left"
+                spacing="sm"
+                icon={
+                  <ThemeIcon color="teal" size={24} radius="xl">
+                    <IconCircleCheck size="1rem" />
+                  </ThemeIcon>
+                }
+              >
+                <List.Item>The world's first truly autonomous dialer</List.Item>
+                <List.Item>Verified calls with YOUR phone number</List.Item>
+                <List.Item>Mobile friendly -- call on the go!</List.Item>
+                <List.Item>
+                  1000 minutes included. Purchase more anytime
+                </List.Item>
+                <List.Item>100% satisfaction guarantee</List.Item>
+              </List>
+            </Box>
+            <Center>
+              <Button
+                variant="gradient"
+                size="xl"
+                onClick={() => navigate(routes.signUp)}
+                mt={32}
+              >
+                Try for free
+              </Button>
+            </Center>
           </Box>
         </Card>
-
-        <Center>
-          <Button
-            variant="gradient"
-            size="xl"
-            onClick={() => navigate(routes.signUp)}
-            mt={32}
-          >
-            Try for free
-          </Button>
-        </Center>
       </Container>
     </FeaturesStyled>
   );
