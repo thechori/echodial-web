31f9668f48efa7865fa289c3fc3b494e5494c9d2
Ryan Teodoro <ryan.h.teodoro@gmail.com>
2023-10-23 00:52:54 -0500

    improve ui/ux for closing and discarding changes in lead edit

--- src/pages/leads/LeadDetail.tsx
+++ src/pages/leads/LeadDetail.tsx
@@ -5,6 +5,7 @@ import {
   Flex,
   Group,
   Select,
+  Stack,
   Text,
   TextInput,
   ThemeIcon,
@@ -17,7 +18,7 @@ import { notifications } from "@mantine/notifications";
 //
 import { LeadDetailStyled } from "./LeadDetail.styles";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useGetLeadStatusesQuery } from "../../services/lead-status";
 import { useUpdateLeadMutation } from "../../services/lead";
 import { useForm } from "@mantine/form";
@@ -26,13 +27,13 @@ import { extractErrorMessage } from "../../utils/error";
 import { PhoneInput } from "../../components/phone-input";
 import { AiOutlineClose } from "react-icons/ai";
 import { setSelectedLead } from "../../store/lead-detail/slice";
+import { useWindowEvent } from "@mantine/hooks";
 
 export const LeadDetail = () => {
   const dispatch = useAppDispatch();
   const { selectedLead } = useAppSelector((state) => state.leadDetail);
 
   const [error, setError] = useState("");
-  // const [lead, setLead] = useState<Lead | null>(null);
   const { data: availableStatuses } = useGetLeadStatusesQuery();
   const [updateLead, { isLoading }] = useUpdateLeadMutation();
 
@@ -60,16 +61,16 @@ export const LeadDetail = () => {
 
   // Close icon
   function handleClose() {
-    cancelEdit();
+    discardChanges();
     dispatch(setSelectedLead(null));
   }
 
   // Cancel edit
-  function cancelEdit() {
+  function discardChanges() {
     form.reset();
     if (!selectedLead)
       return notifications.show({
-        message: "Error editing the lead. Please try again later.",
+        message: "Error editing the selectedLead. Please try again later.",
       });
     form.setValues({
       ...selectedLead,
@@ -78,6 +79,7 @@ export const LeadDetail = () => {
           ? new Date(selectedLead.appointment_at)
           : null,
     });
+    form.resetDirty();
   }
 
   async function editLead() {
@@ -101,17 +103,11 @@ export const LeadDetail = () => {
     }
   }
 
-  useEffect(() => {
-    if (selectedLead) {
-      form.setValues({
-        ...selectedLead,
-        appointment_at:
-          selectedLead && selectedLead.appointment_at
-            ? new Date(selectedLead.appointment_at)
-            : null,
-      });
+  useWindowEvent("keydown", (e) => {
+    if (e.key === "Escape" && selectedLead) {
+      handleClose();
     }
-  }, [selectedLead]);
+  });
 
   return (
     <LeadDetailStyled open={!!selectedLead}>
@@ -130,9 +126,14 @@ export const LeadDetail = () => {
               <Text size="sm">Local time: ??</Text>
             </Box>
 
-            <ThemeIcon color="red" onClick={handleClose}>
-              <AiOutlineClose />
-            </ThemeIcon>
+            <Button
+              color="red"
+              onClick={handleClose}
+              leftIcon={<AiOutlineClose />}
+              variant="outline"
+            >
+              Close
+            </Button>
           </Flex>
         </Box>
 
@@ -178,14 +179,34 @@ export const LeadDetail = () => {
           </Group>
         </Box>
 
-        <Flex justify="center" align="center">
-          <Button onClick={cancelEdit} variant="subtle" mx={4}>
-            Cancel
-          </Button>
-          <Button loading={isLoading} onClick={editLead} mx={4}>
+        <Stack py="lg" spacing="sm">
+          <Button loading={isLoading} onClick={editLead} w={200} mx="auto">
             Update
           </Button>
-        </Flex>
+          {form.isDirty() ? (
+            <Button
+              onClick={discardChanges}
+              variant="subtle"
+              w={200}
+              mx="auto"
+              size="xs"
+              compact
+            >
+              Discard changes
+            </Button>
+          ) : (
+            <Button
+              onClick={handleClose}
+              variant="subtle"
+              w={200}
+              mx="auto"
+              size="xs"
+              compact
+            >
+              Close
+            </Button>
+          )}
+        </Stack>
 
         <Text w="100%" color="red">
           {/*  @ts-ignore */}
