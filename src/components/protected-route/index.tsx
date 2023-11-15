56e7726e678fce7f0b82c3c762a1ce24dca58d3a
Ryan Teodoro <ryan.h.teodoro@gmail.com>
2023-11-15 01:33:48 -0600

    fix logic with protected route component

--- src/components/protected-route/index.tsx
+++ src/components/protected-route/index.tsx
@@ -1,21 +1,15 @@
-import { useNavigate } from "react-router-dom";
-import { notifications } from "@mantine/notifications";
+import { Navigate } from "react-router-dom";
 //
-import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { selectJwt, selectJwtDecoded, signOut } from "../../store/user/slice";
+import { useAppSelector } from "../../store/hooks";
+import { selectJwt } from "../../store/user/slice";
 import routes from "../../configs/routes";
 
 const ProtectedRoute = ({ children }: any) => {
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
   const jwt = useAppSelector(selectJwt);
-  const jwtDecoded = useAppSelector(selectJwtDecoded);
 
   // Handle no JWT
-  if (!jwt || !jwtDecoded) {
-    notifications.show({ message: "No session found. Please log in again" });
-    dispatch(signOut());
-    navigate(routes.signIn);
+  if (!jwt) {
+    return <Navigate to={routes.signIn} />;
   }
 
   return children;
