5af0b12b505ec890a17e08568ae7a480d854d4a9
Ryan Teodoro <ryan.h.teodoro@gmail.com>
2023-12-06 13:07:46 -0600

    improve location of sign in button in on sign up page to avoid it getting lost at bottom

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -118,24 +118,35 @@ function SignUp() {
         <div className="card">
           <div className="left">
             <div className="value-proposition">
-              <div className="title">With {APP_NAME} you get:</div>
-              <List
-                spacing="md"
-                size="md"
-                center
-                icon={
-                  <ThemeIcon color="teal" size={24} radius="xl">
-                    <IconCircleCheck size="1rem" />
-                  </ThemeIcon>
-                }
-              >
-                <List.Item>The dialer with total awareness</List.Item>
-                <List.Item>Best-in-class user interface</List.Item>
-                <List.Item>High-quality voice calls</List.Item>
-                <List.Item>1500+ voice call minutes per month</List.Item>
-                <List.Item>Access to premium support</List.Item>
-                <List.Item>Free trial</List.Item>
-              </List>
+              <Flex direction="column">
+                <div className="title">With {APP_NAME} you get:</div>
+                <List
+                  spacing="md"
+                  size="md"
+                  center
+                  icon={
+                    <ThemeIcon color="teal" size={24} radius="xl">
+                      <IconCircleCheck size="1rem" />
+                    </ThemeIcon>
+                  }
+                >
+                  <List.Item>The dialer with total awareness</List.Item>
+                  <List.Item>Best-in-class user interface</List.Item>
+                  <List.Item>High-quality voice calls</List.Item>
+                  <List.Item>1500+ voice call minutes per month</List.Item>
+                  <List.Item>Access to premium support</List.Item>
+                  <List.Item>Free trial</List.Item>
+                </List>
+              </Flex>
+
+              <Box>
+                <Text size="sm" pb={4}>
+                  Already have an account?
+                </Text>
+                <Button compact onClick={() => navigate(routes.signIn)}>
+                  Sign In
+                </Button>
+              </Box>
             </div>
           </div>
           <div className="right">
@@ -239,13 +250,6 @@ function SignUp() {
             </form>
           </div>
         </div>
-
-        <Flex align="center" justify="space-between" w={300} py={32} mx="auto">
-          <Text color="white">Already have an account?</Text>
-          <Button variant="white" onClick={() => navigate(routes.signIn)}>
-            Sign In
-          </Button>
-        </Flex>
       </Container>
     </SignUpStyled>
   );
